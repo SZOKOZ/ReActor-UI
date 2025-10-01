7c6c457533489fd1c0d828a3853d62b57e8b58f4
szokoz <epiclegend12@live.co.uk>
2025-10-01 23:30:31 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
-
+import sys
+import types
+from torchvision.transforms.functional import rgb_to_grayscale
 from modules import core
+
+# Create a module for `torchvision.transforms.functional_tensor`
+functional_tensor = types.ModuleType("torchvision.transforms.functional_tensor")
+functional_tensor.rgb_to_grayscale = rgb_to_grayscale
+
+# Add this module to sys.modules so other imports can access it
+sys.modules["torchvision.transforms.functional_tensor"] = functional_tensor
 
 if __name__ == '__main__':
     core.run()
